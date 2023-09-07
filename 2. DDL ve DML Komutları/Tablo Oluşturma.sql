5adb7233bdf0e89c3fb225edba80bd30cc0b941d
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-07 16:41:04 +0300

    DDL Komutları Alter ve Drop Kullanıldı

--- 2. DDL ve DML Komutları/Tablo Oluşturma.sql
+++ 2. DDL ve DML Komutları/Tablo Oluşturma.sql
@@ -1,5 +1,18 @@
+--Tablo oluþturmak için kullanýlýr
 CREATE TABLE tbl_lessons
 (
 lesson_id tinyint,
 lesson_name varchar(20)
 )
+
+--Tablo'ya yeni Column eklemek için kullanýlýr
+ALTER TABLE tbl_lessons
+ADD quota smallint
+
+--Tablo'dan bir Column silmek için kullanýlýr
+ALTER TABLE tbl_lessons
+drop column quota
+
+--Tablo'yu tamamen silmek için kullanýlýr
+DROP TABLE tbl_lessons
+
