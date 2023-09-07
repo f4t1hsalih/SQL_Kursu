97d175be7137303e6b19bd72b681c82377d58132
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-07 18:01:15 +0300

    DML Komutları ile Delete kullanımı

--- 2. DDL ve DML Komutları/DML Komutları.sql
+++ 2. DDL ve DML Komutları/DML Komutları.sql
@@ -1,3 +1,4 @@
+--SELECT KOMUTU
 --Bütün tabloyu getirmeye yarar
 select * from tbl_students
 
@@ -16,7 +17,14 @@ select * from tbl_students where std_city='Adana' and std_club='Kitaplýk'
 --Or birden fazla koþulun her birinin olabileceði durumda kullanýlýr
 select * from tbl_students where std_city='Adana' or std_club='Satranç'
 
+--INSERT KOMUTU
 --Insert Into ile tabloya veri eklendi
 insert into tbl_students(std_name, std_surname, std_city) values ('Baran', 'Yücedað', 'Elazýð')
 insert into tbl_notes (std_id, lesson_id, exam1, exam3) values (6, 2, 88, 67)
 
+--DELETE KOMUTU
+--Delete komutu ile tüm dersleri siler
+delete from tbl_lessons
+
+--id numarasý 6 olan dersi siler
+delete from tbl_lessons where lesson_id = 6
