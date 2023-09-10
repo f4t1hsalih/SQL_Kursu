c2f44154e8f32a816aa1e567688be91868e4684a
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-10 12:02:20 +0300

    Like işaretleri kullanımı

--- 3. Gruplandırmalar ve Operatörler/Like Kullanımı.sql
+++ 3. Gruplandırmalar ve Operatörler/Like Kullanımı.sql
@@ -7,3 +7,18 @@ select * from tbl_students where std_name like 'a%'
 
 --Ýçerisinde a harfi geçirmeyen kulüpleri getirir
 select * from tbl_students where std_club not like '%a%'
+
+--Ýçerisinde an geçecek fakat devamý farklý olacak þehirleri getirir
+select * from tbl_students where std_city like '%ir_%'
+
+--Ýsmi a ya da b ile baþlayan öðrencileri getirir
+select * from tbl_students where std_name like '[A,B]%'
+
+--Ýsminde a ya da b geçen öðrencileri getirir
+select * from tbl_students where std_name like '%[A,B]%'
+
+--Ýsminin son harfi n ya da ý olan öðrencileri getirir
+select * from tbl_students where std_name like '%[N,I]'
+
+--Clübün adý a ile h arasýndaki harflerden baþlýyor ise o kulüpleri getirir
+select * from tbl_students where std_club like '[a-h]%'
