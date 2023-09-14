3b7d53cf005d5be52935ee024be69ae00e04cde0
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-14 15:03:57 +0300

    Matematiksel Fonksiyonların Kullanımı

---
+++ 6. Aritmetik Fonksiyonlar/Matematiksel Fonksiyonlar.sql
@@ -0,0 +1,21 @@
+--MUTLAK DEÐER (ABS) KULLANIMI
+--Abs kullanarak negatif bir deðeri pozitif olarak gösterir
+select ABS(-92) as 'Mutlak'
+
+--ÜS ALMA (POWER) KULLANIMI
+--Power komutu ile üs alma iþlemi yapar
+select POWER(3, 3) as 'Üs'
+
+--Sýnav1 deki notlarýn karesini alýr
+select power(exam1,2) from tbl_notes
+
+--KAREKÖK BULMA (SQRT) KULLANIMI
+--Sqrt komutu ile karekök bulma iþlemi yapar
+select SQRT(144) as 'Karekök'
+
+--TABANA ve TAVANA YUVARLAMA
+--Floor komutu ile tabana yuvarlar
+select FLOOR(4.86) as 'Taban'
+
+--Ceiling komutu ile tavana yuvarlar
+select CEILING(4.86) as 'Tavan'
