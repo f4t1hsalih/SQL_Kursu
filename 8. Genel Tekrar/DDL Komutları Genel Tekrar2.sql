0808a1e4a856e2bd84d236d8422671b85b26a547
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-16 21:28:21 +0300

    DDL Komutları Genele Tekrar 2

---
+++ 8. Genel Tekrar/DDL Komutları Genel Tekrar2.sql
@@ -0,0 +1,9 @@
+--Veri Tabanýna Veri Kaydetme
+
+insert into tbl_categories (category_name) values ('Bilgisayar')
+insert into tbl_categories (category_name) values ('Beyaaz eþya')
+insert into tbl_categories (category_name) values ('Küçük Ev Aleyleri')
+insert into tbl_categories (category_name) values ('TV')
+
+insert into tbl_products(product_name, product_brand, category_id, product_bprice, product_sprice, product_stock)
+values ('Buzdolabý', 'Arçelik', 2, 10250, 12000, 15)
