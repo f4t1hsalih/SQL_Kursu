fc3d15ca45e521307fb6d34d1547f5d4463787f8
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-18 13:43:34 +0300

    Alt Sorgu Örnekleri Devamı

--- 11. Alt Sorgu Örnekleri/Alt Sorgu Örnekleri.sql
+++ 11. Alt Sorgu Örnekleri/Alt Sorgu Örnekleri.sql
@@ -4,3 +4,10 @@ select * from tbl_movement where product_id in (select product_id from tbl_produ
 
 --Hareket tablosu içinde bulunan bilgisayar kategorisine ait bilgileri getirir(2 alt sorgu ile)
 select * from tbl_movement where product_id in (select product_id from tbl_products where category_id=(select category_id from tbl_categories where category_name='Bilgisayar'))
+
+--Þehri adana olan müþterilerin yaptýðý hareketleri görüntüleyen tablo
+select * from tbl_movement where customer_id in (select customer_id from tbl_customer where customer_city='Adana')
+
+--Beyaz eþya kategorindeki ürünlere ait hareketleri getirir
+select * from tbl_movement where product_id in (select product_id from tbl_products where category_id=(select category_id from tbl_categories where category_name='Beyaz Eþya'))
+
