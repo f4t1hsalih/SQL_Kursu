4d291bb4f22517e44e03538c53dc087ba683b5f3
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-18 14:09:53 +0300

    Alt sorgu örnekleri devamı

--- 11. Alt Sorgu Örnekleri/Alt Sorgu Örnekleri.sql
+++ 11. Alt Sorgu Örnekleri/Alt Sorgu Örnekleri.sql
@@ -11,3 +11,18 @@ select * from tbl_movement where customer_id in (select customer_id from tbl_cus
 --Beyaz eþya kategorindeki ürünlere ait hareketleri getirir
 select * from tbl_movement where product_id in (select product_id from tbl_products where category_id=(select category_id from tbl_categories where category_name='Beyaz Eþya'))
 
+--Adana ve Ankaradaki müþterilerin toplam yaptýðý alýþveriþin tutarý
+select sum(amount) from tbl_movement where customer_id in (select customer_id from tbl_customer where customer_city='Adana' or customer_city = 'Ankara')
+
+--Satýlan ürünlerin sayýsýnýn ürün tablosundan düþmesi için update sorgusu
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=1) where product_id=1
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=2) where product_id=2
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=3) where product_id=3
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=4) where product_id=4
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=5) where product_id=5
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=6) where product_id=6
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=7) where product_id=7
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=8) where product_id=8
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=9) where product_id=9
+update tbl_products set product_stock=product_stock-(select sum(piece) from tbl_movement where product_id=10) where product_id=10
+
