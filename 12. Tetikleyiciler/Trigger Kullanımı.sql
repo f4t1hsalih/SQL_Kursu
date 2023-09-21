1113d301e0ed3fc8b44b0c83f7b4358fd2a09b9a
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-21 15:11:13 +0300

    Trigger ile adet sayısını artırma

--- 12. Tetikleyiciler/Trigger Kullanımı.sql
+++ 12. Tetikleyiciler/Trigger Kullanımı.sql
@@ -5,3 +5,11 @@ on tbl_movement    --Hareket tablosunda çalışacak
 after INSERT       --Insert yapıldıktan sonra
 AS
 UPDATE tbl_counter set process=process+1
+
+--Trigger kullanarak kategoriler tablosuna her yeni kayıt eklendiğinde toplamKategori tablosundaki adet isimli stunun değeri 1 artacak
+CREATE TRIGGER kategoriartis
+on tbl_categories    --Kategoriler tablosunda çalışacak
+after INSERT         --Insert yapıldıktan sonra
+AS
+UPDATE tbl_totalCategory set piece+=+1
+
