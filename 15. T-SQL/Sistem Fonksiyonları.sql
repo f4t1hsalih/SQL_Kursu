f6d82555144d393c35c7c7ec2de4426877c4dbc9
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-26 12:42:18 +0300

    Sistem Fonksiyonları

---
+++ 15. T-SQL/Sistem Fonksiyonları.sql
@@ -0,0 +1,21 @@
+--SQL Sistem Fonksiyonlarý
+
+--Sunucu ismini verir
+select @@SERVERNAME
+
+--Son hata alýnan satýrýn deðeri
+select @@ERROR
+
+--Varsayýlan dil id'si
+select @@DEFAULT_LANGID
+--Kullanýlan dil
+select @@LANGUAGE
+
+--Maximum baðlantý sayýsý
+select @@MAX_CONNECTIONS
+
+--Microsoft versiyonu
+select @@MICROSOFTVERSION
+
+--SQL Server versiyonu
+select @@VERSION
