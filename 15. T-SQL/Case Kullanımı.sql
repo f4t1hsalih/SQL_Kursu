d5064a4e6b965854fa6bf22c24f746a9dd504e0a
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-26 13:50:08 +0300

    Case Kullanımı

---
+++ 15. T-SQL/Case Kullanımı.sql
@@ -0,0 +1,35 @@
+--CASE KULLANIMI
+
+--3 Yöntemlede yapýlabilir
+--Ürünler tablosunda ürün durumu 1 olan ürünlere ürün var 0 olan ürünlere ise ürün yok yazmaya yarar
+select product_name, product_brand,
+case product_status
+when '1' then 'Ürün var'
+when '0' then 'Ürün yok'
+end as 'Ürün Durum'
+from tbl_products
+
+select product_name, product_brand,
+case 
+when product_status = 1 then 'Ürün var'
+when product_status = 0 then 'Ürün yok'
+end as 'Ürün Durum'
+from tbl_products
+
+select product_name, product_brand,
+case product_status
+when 1 then 'Ürün var'
+when 0 then 'Ürün yok'
+end as 'Ürün Durum'
+from tbl_products
+
+--Ürün kategorilerini Case ile getirme
+--Else ile diðer seçenekler yerine 'BÝLÝNMEYEN' yazdýrýldý
+select product_name, product_brand,
+case category_id
+when 1 then 'LAPTOP'
+when 2 then 'BEYAZ EÞYA'
+when 3 then 'KÜÇÜK EV ALETLERÝ'
+else 'BÝLÝNMEYEN'
+end
+from tbl_products
