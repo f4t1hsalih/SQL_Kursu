459cca9095a4a13cd133419f5f091073e77e69cf
f4t1hsalih <f4t1hsalih@gmail.com>
2023-10-05 00:10:10 +0300

    Anaform da chart'a veri çekme

--- 16. Somut Proje/SQL_Learning/SQL_Learning/FrmMain.cs
+++ 16. Somut Proje/SQL_Learning/SQL_Learning/FrmMain.cs
@@ -35,12 +35,25 @@ namespace SQL_Learning
 
         private void FrmMain_Load(object sender, EventArgs e)
         {
+            //Prosedür ile kritik seviyeye düşen ürünleri getirir
             string command = "Exec test4";
             SqlCommand cmd = new SqlCommand(command, connection);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             da.Fill(dt);
             dataGridView1.DataSource = dt;
+
+            //Kategorilerin adlarını ve o kategoriye ait ürünlerin adetlerini charta çeker
+            connection.Open();
+            string command2 = "select category_name, COUNT(*) from tbl_categories C inner join tbl_products P on C.category_id=P.category_id group by category_name";
+            SqlCommand cmd2 = new SqlCommand(command2, connection);
+            SqlDataReader dr = cmd2.ExecuteReader();
+            while (dr.Read())
+            {
+                chart1.Series["Kategoriler"].Points.AddXY(dr[0], dr[1]);
+            }
+            dr.Close();
+            connection.Close();
         }
     }
 }
