9f43c7286e1da7d1bdad9133b4168b95ac1096d0
f4t1hsalih <f4t1hsalih@gmail.com>
2023-10-11 13:52:34 +0300

    statistic komutları ile istatistikler bulunundu

--- 21. Index/Index Kullanımı.sql
+++ 21. Index/Index Kullanımı.sql
@@ -15,3 +15,23 @@ insert tbl_tablo1
 select 'Kitap' + CAST(@sayac as varchar(20))
 set @sayac = @sayac + 1
 end
+
+select * from tbl_tablo1
+
+--SET STATISTICS IO ON:
+--Bu komut, sorgu sýrasýnda gerçekleþen disk giriþ/çýkýþ (I/O) 
+--iþlemlerini gösteren istatistikleri etkinleþtirir. 
+--Bu istatistikler, sorgunun çalýþtýrýlmasý sýrasýnda 
+--disk üzerinde kaç okuma ve yazma iþlemi gerçekleþtiðini gösterir.
+
+
+--SET STATISTICS TIME ON:
+--Bu komut, sorgunun çalýþtýrýlmasý süresine iliþkin istatistikleri etkinleþtirir.
+--Bu istatistikler, sorgunun baþlama ve bitiþ zamanýný gösterir ve toplam 
+--süreyi milisaniye cinsinden raporlar.
+
+set statistics io on
+set statistics time on
+
+select * from tbl_tablo1
+select * from tbl_tablo1 where id = 20000
